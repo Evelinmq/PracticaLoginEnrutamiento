163eff8bee852ef24309d33d52de58f06cb080b4
SanDaniel777 <20243ds156@utez.edu.mx>
2026-03-04 08:49:13 -0600

    creacion del arreglo de usuarios

---
+++ src/pages/users/users.js
@@ -0,0 +1,34 @@
+const users = [
+    {
+        user: "kevin123",
+        name: "Kevin Daniel Sandoval Rueda",
+        role: "admin",
+        password: "1234"
+    },
+    {
+        user: "getse01",
+        name: "Getsemani Cruz Millan",
+        role: "user",
+        password: "1234"
+    },
+    {
+        user: "andrea01",
+        name: "Andrea Aragon Marquez",
+        role: "user",
+        password: "1234"
+    },
+    {
+        user: "ewelin01",
+        name: "Evelin Mojica Quintana",
+        role: "admin",
+        password: "1234"
+    },
+    {
+        user: "diego01",
+        name: "Diego Emiliano Reyes Recillas",
+        role: "admin",
+        password: "1234"
+    }
+];
+
+export default users;
